88ace1ced4ae983211e139074288d0042b752e67
kasmr <alikkras@gmail.com>
2021-01-29 00:08:00 +0300

    solved problem with fetching cart

--- src/App.jsx
+++ src/App.jsx
@@ -34,8 +34,8 @@ const App = () => {
   return (
     <div>
       <Navbar totalItems={cart.total_items} />
-      <Products products={products} onAddToCart={handleAddToCart} />
-      {/* <Cart cart={cart} /> */}
+      {/* <Products products={products} onAddToCart={handleAddToCart} /> */}
+      <Cart cart={cart} />
     </div>
   );
 };
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,59 +1,64 @@
-import { Button, Container, Grid, Typography } from "@material-ui/core";
 import React from "react";
+import { Container, Typography, Button, Grid } from "@material-ui/core";
 
 import useStyles from "./styles";
 
 const Cart = ({ cart }) => {
   const classes = useStyles();
+  const isEmpty = !cart.line_items.length;
 
+  const EmptyCart = () => (
+    <Typography variant="subtitle1">
+      Ваша корзина пуста! Добавьте что-нибудь!
+    </Typography>
+  );
+
+  if (!cart.line_items) return "Loading";
+
+  const FilledCart = () => (
+    <>
+      <Grid container spacing={3}>
+        {cart.line_items.map((lineItem) => (
+          <Grid item xs={12} sm={4} key={lineItem.id}>
+            <div>{lineItem.name}</div>
+          </Grid>
+        ))}
+      </Grid>
+      <div className={classes.cardDetails}>
+        <Typography variant="h4">
+          Итого: {cart.subtotal.formatted_with_symbol}
+        </Typography>
+        <div>
+          <Button
+            className={classes.emptyButton}
+            size="large"
+            type="button"
+            variant="contained"
+            color="secondary"
+          >
+            Очистить
+          </Button>
+          <Button
+            className={classes.checkoutButton}
+            size="large"
+            type="button"
+            variant="contained"
+            color="primary"
+          >
+            Заказать
+          </Button>
+        </div>
+      </div>
+    </>
+  );
   return (
     <Container>
       <div className={classes.toolbar} />
-      <Typography className={classes.title} variant="h3">
+      <Typography className={classes.title} variant="h3" gutterBottom>
         Корзина
       </Typography>
-      {cart ? (
-        <>
-          <Grid container spacing={3}>
-            {cart.line_items.map((item) => (
-              <Grid items xs={12} sm={4} key={item.id}>
-                <div>{item.name}</div>
-              </Grid>
-            ))}
-          </Grid>
-          <div className={classes.cardDetails}>
-            <Typography variant="h4">
-              Итого: {cart.subtotal.formatted_with_symbol}
-              <div>
-                <Button
-                  className={classes.emptyButton}
-                  size="large"
-                  type="button"
-                  variant="contained"
-                  color="secondary"
-                >
-                  Пустая корзина
-                </Button>
-                <Button
-                  className={classes.checkoutButton}
-                  size="large"
-                  type="button"
-                  variant="contained"
-                  color="primary"
-                >
-                  checkout
-                </Button>
-              </div>
-            </Typography>
-          </div>
-        </>
-      ) : (
-        <Typography variant="subtitle1">
-          Ваша корзина пустая, добавьте что-нибудь!
-        </Typography>
-      )}
+      {isEmpty ? <EmptyCart /> : <FilledCart />}
     </Container>
   );
 };
-
 export default Cart;
